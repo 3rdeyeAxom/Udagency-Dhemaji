15dfea96e59233bd4aa59ef5aaf832344de41f91
Utpal Das <84780114+3rdeyeAxom@users.noreply.github.com>
2023-11-02 04:22:45 +0900

    swiper__test__1

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,18 +14,36 @@ navToggler.addEventListener("click", function (){
 // Swipper Js
 
 var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
+  slidesPerView: 1,
+  spaceBetween: 10,
+  centeredSlides: false,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  // navigation: {
+  //   nextEl: ".swiper-button-next",
+  //   prevEl: ".swiper-button-prev",
+  // },
+
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
     },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 40,
     },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 50,
     },
-  });
+  }
+  
+
+});
